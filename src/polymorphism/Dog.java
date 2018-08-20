ab15465a1a89ba249737d8642a4bb5836299e780
odessabear@gmail.com <odessabear@gmail.com>
2018-08-20 18:39:08 +0300

    classes and objects lessons, polymorphism

---
+++ src/polymorphism/Dog.java
@@ -0,0 +1,12 @@
+package polymorphism;
+
+public class Dog extends Animal{
+
+    public void eat(){
+        System.out.println("Dog is eating");
+    }
+    public void bark(){
+        System.out.println("Dog barks");
+    }
+
+}
---
+++ src/polymorphism/Test.java
@@ -0,0 +1,9 @@
+package polymorphism;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Animal animal = new Dog();
+        animal.eat();
+    }
+}
